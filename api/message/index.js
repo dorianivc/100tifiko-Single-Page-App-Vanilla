2143ed4ef0d0d6c866d159e356100f2482451596
Dorian Vallecillo <dorianivc1@gmail.com>
2021-07-11 22:04:13 -0600

    api loading with 3rd party.....

--- api/message/index.js
+++ api/message/index.js
@@ -1,8 +1,9 @@
 module.exports = async function (context, req) {
-    fetch('https://rick-and-morty-api-phrases.herokuapp.com//phrases/en_us/random')
-  .then(response => response.json())
-  .then(data => console.log(data));
+    let response;
+    fetch('https://rick-and-morty-api-phrases.herokuapp.com/phrases/en_us/random').then(response => response.json()).then(data => response=data).catch(console.log("error"));
     context.res.json({
-        text: response.phrase
+        text: response.phrase,
+        author: response.author,
+        img: response.img
     });
 };
