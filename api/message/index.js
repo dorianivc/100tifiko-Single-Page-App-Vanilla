326686655ef41569bf0ac3101ea54da47d6b2ab7
Dorian Isaac Vallecillo Calderon <dorianivc1@gmail.com>
2021-07-23 21:01:53 +0000

    Rollback

--- api/message/index.js
+++ api/message/index.js
@@ -1,6 +1,6 @@
+  
 module.exports = async function (context, req) {
     const fetch= require('node-fetch');
-
     let response;
     await fetch('https://rick-and-morty-api-phrases.herokuapp.com/phrases/en_us/random').then(response => response.json()).then(data => response=data).catch(console.log("error"));
     context.res.json({
